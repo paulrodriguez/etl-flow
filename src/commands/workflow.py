dc83eeee9e38f773b54291fec9c37c2e116b3c60
Paul Rodriguez <paul.d.rodriguez@outlook.com>
2021-03-06 22:53:53 -0800

    feat: aggregator container

--- src/commands/workflow.py
+++ src/commands/workflow.py
@@ -3,6 +3,7 @@ import fire
 import yaml
 from src.Util import Util
 from src.containers.SubprocessContainer import SubprocessContainer
+from dependency_injector import containers, providers
 
 class Workflow(object):
   """Workflow class"""
@@ -13,7 +14,16 @@ class Workflow(object):
           if name in data:
               # TODO: parse workflow here
               pass
+  def test(self):
+      obj = subprocess_container.test_factory()
+      print(obj)
+
+      obj2 = subprocess_container.test2_factory()
+      print(obj2)
 
 if __name__ == '__main__':
+
+    SubprocessContainer.test_factory = providers.Object(10)
+    setattr(SubprocessContainer, 'test2_factory', providers.Object(20))
     subprocess_container = SubprocessContainer.get_object()
     fire.Fire(Workflow)
